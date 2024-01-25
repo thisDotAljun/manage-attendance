fe608ef99378a22f58d92ccf455e9636b2b8d940
this.Aljun <cortinaaljun@gmail.com>
2024-01-25 18:33:35 +0800

    Add dependecies and setup configuration

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
 
 import App from './App.vue'
 import router from './router'
 
+import 'primeicons/primeicons.css'
+import 'primevue/resources/themes/md-light-indigo/theme.css'
+
 const app = createApp(App)
+app.use(PrimeVue)
 app.use(createPinia())
 app.use(router)
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js}'],
+  content: ['./index.html', "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        "light-neutral" : "#FFFFFF",
+        "light-neutral-40" : "#C3CDC9", 
+        "light-neutral-80" : "#879A94",
+        "mid-neutral-200" : "#5A756B",
+        "mid-neutral-90" : "#788E86",
+        "mid-neutral-400" : "#3C5B51",
+        "dark-neutral-500" : "#2D4F43",
+        "dark-neutral-800" : "#00291B",
+        "success-bg" : "#E6FFEE",
+        "green" : "#17AD49", 
+        "blue" : "#0F6EEB",
+        "header-color" : "#333333",
+        "nav-color" : "#DDDDDD"
+      }
+    }
   },
   plugins: [],
   corePlugins: {
